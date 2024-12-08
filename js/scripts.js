4046e7c35b0bb2edec93b32d4c20a48b790aa4d7
Brian Bauska <brianbauska@gmail.com>
2024-12-08 14:15:23 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,8 @@ let counter = 10100;  /* from Dec 7th-8th */
 function count() {
   counter++;
   document.getElementById('number').innerHTML = counter;
-  }
-  /* following was = count; */
-  document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById('clicker').onclick = count;
-	})
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+document.getElementById('clicker').onclick = count;
+})
