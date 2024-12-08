55446300a4962aad72c8ed9964498a7905ba6d6d
Brian Bauska <brianbauska@gmail.com>
2024-12-08 13:12:56 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 <!-- ./js/scripts.js in Silus-counter.bauska.org -->
-let counter = 10100;  /* from Dec 7th/8th */
+let counter = 10100;  /* from Dec 7th-8th */
 
 function count() {
   counter++;
   document.getElementById('number').innerHTML = counter;
   }
-  
+  /* following was = count; */
   document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById('clicker').onclick = count;
+    document.getElementById('clicker').onclick = counter;
 	})
