4ab5c6596153f46e4d67c348961211d581a349e3
Brian Bauska <brianbauska@gmail.com>
2024-12-14 01:28:31 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 <!-- ./js/scripts.js in Silus-clicker.bauska.org -->
-let counter = 10010;  /* from Dec 7th-8th */
+let counter = 10010 + 24215;  /* from Dec 7th-8th (6 hours) & Dec 13-14 (10 hours) */
 
 function count() {
   counter++;
