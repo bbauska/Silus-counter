ef8c20351b4093ba410a05c421bcb446b219d997
Brian Bauska <brianbauska@gmail.com>
2024-12-22 18:10:29 -0800

    Update confetti.js

--- js/confetti.js
+++ js/confetti.js
@@ -1,1 +1,12 @@
-🥩
+import JSConfetti from 'js-confetti'
+const jsConfetti = new JSConfetti()
+
+jsConfetti.addConfetti({
+  emojis: ['🥩'],
+  emojiSize: 100,
+  confettiNumber: 30,
+})
+
+// ... 
+jsConfetti.clearCanvas()
+
