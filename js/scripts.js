3824a478ae7c52ee4a438ba98309b8efa82da6c4
Brian Bauska <brianbauska@gmail.com>
2024-12-25 20:06:08 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -10,23 +10,4 @@ function count() {
 
 document.addEventListener('DOMContentLoaded', function(){
   document.getElementById('clicker').onclick = count;
-  let confettiCounter = count / 100
-  if confettiCounter.isInteger  {
-  showConfetti();
 })
-
-function SwapDivsWithClick(div1,div2)
-{
-   d1 = document.getElementById(div1);
-   d2 = document.getElementById(div2);
-   if( d2.style.display == "none" )
-   {
-      d1.style.display = "none";
-      d2.style.display = "block";
-   }
-   else
-   {
-      d1.style.display = "block";
-      d2.style.display = "none";
-   }
-}
