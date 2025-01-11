cc1d9faec5ec4051eb3c9a2b58ae6d8d4056d752
Brian Bauska <brianbauska@gmail.com>
2025-01-10 17:24:20 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 <!-- ./js/scripts.js in Silus-clicker.bauska.org -->
-let counter = 10010 + 24215 + 55855;  /* from Dec 7th-8th (6 hours), Dec 13-14 (10 hours), Dec 20, 21, 22 (16 hours) */
+let counter = 10010 + 24215 + 55855;  /* from Dec 7th-8th (6 hours - 1 day), Dec 13-14 (10 hours - 1 day), Dec 20, 21, 22 (16 hours - 2 days), Jan 10-11, 2025 (6pm-4:50am) */
 
 function count() {
   counter++;
