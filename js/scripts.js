c151cb644714ab2763cbb7c415f167258a28843d
Brian Bauska <brianbauska@gmail.com>
2025-01-22 17:21:03 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -22,20 +22,3 @@ function count() {
 document.addEventListener('DOMContentLoaded', function(){
   document.getElementById('clicker').onclick = count;
 })
-
-const confettiContainer = document.querySelector('#confetti-container');
-const showConfetti = () => {
-  const confetti = document.createElement('div');
-  confetti.textContent = '🥩';
-  confetti.classList.add('confetti');
-  confetti.style.left = Math.random() * innerWidth + 'px';
-  confettiContainer.appendChild(confetti);
-
-  setTimeout(() => {
-    confetti.remove();
-  }, 5000);  /* 5000 - how far down the screen before disappears */
-};
-
-setInterval(() => {
-  showConfetti();
-}, 100);  /* 400 */
