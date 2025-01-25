b1c44fa3147edaf8f3f165e7e5687102e86b858c
Brian Bauska <brianbauska@gmail.com>
2025-01-25 11:03:43 -0800

    Update Confetti.js

--- js/Confetti.js
+++ js/Confetti.js
@@ -3,12 +3,13 @@ const showConfetti = () => {
   const confetti = document.createElement('div');
   confetti.textContent = '🥩';
   confetti.classList.add('confetti');
+  innerWidth = innerWidth - 100;
   confetti.style.left = Math.random() * innerWidth + 'px';
   confettiContainer.appendChild(confetti);
 
   setTimeout(() => {
     confetti.remove();  /* confetti.remove */
-  }, 3000);  /* wait 5 seconds, 5000 */
+  }, 3000);  /* wait 3 seconds, was 5000 */
 };
 
 setInterval(() => {
