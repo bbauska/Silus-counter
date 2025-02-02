ee537fa215e3af4761a0e589bcefe1726eaacc1f
Brian Bauska <brianbauska@gmail.com>
2025-02-02 14:54:44 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,9 @@
    Jan 24, 2025 = 12150 (10 hours)
    Jan 25, 2025 = 32057 (10 hours) 
    Jan 31, 2025 = 8993 (4 hours) 
-   Feb 1, 2025 = 42700 (10 hours) */
-let counter = 10010 + 24215 + 55855 + 44920 + 36000 + 45350 + 12150 + 32057 + 8993 + 42700;
+   Feb 1, 2025 = 42700 (10 hours) 
+   Feb 2, 2025 = 7,900 (2 hours) */
+let counter = 10010 + 24215 + 55855 + 44920 + 36000 + 45350 + 12150 + 32057 + 8993 + 42700 + 7900;
 /* 10010 from Dec 7th-8th (6 hours - 1 day), 
    24215 from Dec 13-14 (10 hours - 1 day), 
    55855 from Dec 20, 21, 22 (16 hours - 2 days), 
@@ -18,7 +19,8 @@ let counter = 10010 + 24215 + 55855 + 44920 + 36000 + 45350 + 12150 + 32057 + 89
    12150 from Jan 24, 2025 (10 hrs) 
    32057 from Jan 25, 2025 (10 hrs) 
    8993 from Jan 31, 2025 (4 hrs) 
-   42700 from Feb 1, 2025 all times are approximate. */
+   42700 from Feb 1, 2025 (10 hours) 
+   7,900 from Feb 2, 2025 (2 1/2 hours) all times are approximate. */
 
 function count() {
   counter++;
